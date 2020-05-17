e4193878b3d18b5ddc0ba50cb8bac07f478ad20b
Igor Sena <gunsigoroses@gmail.com>
2020-05-16 23:05:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Veiculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo v1 = new Veiculo ();
+            v1.SetPlaca("ABC-1234");
+            v1.SetCombustivel(255);
+            Console.WriteLine("Placa V1: " + v1.GetPlaca());
+            Console.WriteLine("Combustível dessa merda V1: " + v1.GetCombustivel());
+
+            Veiculo v2 = new Veiculo();
+            v2.SetPlaca("BCT-069");
+            v2.SetCombustivel(999);
+            Console.WriteLine("Placa V2: " + v2.GetPlaca());
+            Console.WriteLine("Combustível dessa merda V2: " + v2.GetCombustivel());
+
+            Veiculo v3 = new Veiculo();
+            v3.SetPlaca("XXT-0022");
+            v3.SetCombustivel(999);
+            Console.WriteLine("Placa V3: " + v3.GetPlaca());
+            Console.WriteLine("Combustível dessa merda V3: " + v3.GetCombustivel());
+        }
+    }
+}
---
+++ Veiculo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veiculo
+{
+    class Veiculo
+    {
+        private String Placa;
+        private int Combustivel;
+
+        public Veiculo()
+        {
+
+        }
+
+        public Veiculo (int Combustivel)
+        {
+            this.Combustivel = Combustivel;
+        }
+        public void SetPlaca (string valor)
+        {
+            this.Placa = valor;
+        }
+        public string GetPlaca()
+        {
+            return this.Placa;
+        }
+
+        public void SetCombustivel(int Combustivel)
+        {
+            this.Combustivel = Combustivel;
+        }
+        public string GetCombustivel()
+        {
+            return this.Placa;
+        }
+    }
+}
